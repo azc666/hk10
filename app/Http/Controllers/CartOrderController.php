a76c9a1626ff60283d38ad4069e2f7604b98d1d7
Allen Cohen <azc666@gmail.com>
2021-02-27 18:58:35 -0500

    addend ntag email redipients

--- app/Http/Controllers/CartOrderController.php
+++ app/Http/Controllers/CartOrderController.php
@@ -475,12 +475,12 @@ class CartOrderController extends Controller
         }
         if ($prod_layout == 'NTAG') {
             \Mail::to('sheri.testa@hklaw.com')->send(new OrderConfirmEmail($cartOrderEmail));
-            // \Mail::to('Sonia.tsunis@hklaw.com')->send(new OrderConfirmEmail($cartOrderEmail));
-            // \Mail::to('Sarah.caramanica@hklaw.com')->send(new OrderConfirmEmail($cartOrderEmail));
+            \Mail::to('Sonia.tsunis@hklaw.com')->send(new OrderConfirmEmail($cartOrderEmail));
+            \Mail::to('Sarah.caramanica@hklaw.com')->send(new OrderConfirmEmail($cartOrderEmail));
         }
 
-        // \Mail::to('output@g-d.com')->send(new OrderProductionEmail($cartOrderProduction, $order));
-        // \Mail::to('output@g-d.com')->send(new OrderConfirmEmail($cartOrderEmail));
+        \Mail::to('output@g-d.com')->send(new OrderProductionEmail($cartOrderProduction, $order));
+        \Mail::to('output@g-d.com')->send(new OrderConfirmEmail($cartOrderEmail));
 
             // \Mail::to('austin@g-d.com')->send(new OrderProductionEmail($cartOrderProduction, $order));
             \Mail::to('tmann999@gmail.com')->send(new OrderProductionEmail($cartOrderProduction, $order));
